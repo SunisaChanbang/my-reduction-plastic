f6e9c8a7b9fd7afb47c0901fbcf3b31d7b6cf5e0
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2021-04-30 05:37:52 +0700

    up

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
-  
-  $(".show_all .link").click(function(){
+$(document).ready(function () {
+  $(".show_all .link").click(function () {
     $(".notifications").removeClass("active");
     $(".popup").show();
   });
 
-  $(".close").click(function(){
+  $(".close").click(function () {
     $(".popup").hide();
   });
 });
 
 function editName(x) {
-  console.log(x)
-      x.classList.toggle("fa-save");
-      x.classList.toggle("fa-edit");
+  console.log(x);
+  x.classList.toggle("fa-save");
+  x.classList.toggle("fa-edit");
+  var y = document.getElementById("fname").readOnly;
+  if (y == true) {
+    $("input").prop("readonly", false);
+    document.getElementById("fname").classList.toggle("text-end")
+  } else {
+    $("input").prop("readonly", true);
+    document.getElementById("fname").classList.toggle("text-end")
+  }
 }
+
