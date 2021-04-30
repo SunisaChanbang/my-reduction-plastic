e877c404bffc1b2f5b75ab12c5d4089b60b5a37a
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2021-04-30 19:03:19 +0700

    up

--- javascript/script.js
+++ javascript/script.js
@@ -23,57 +23,34 @@ function editName(x) {
   }
 }
 
-// ///////////////////////////////////
+
 // Start upload preview image
-$("#choose_image").attr("src", "/pictures/_MG_8982.jpg");
-var $uploadCrop,
-  tempFilename,
-  rawImg,
-  imageId;
-function readFile(input) {
+function readURL(input) {
   if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $('#upload-demo').addClass('ready');
-      $('#uploadimageModal').modal('show');
-      rawImg = e.target.result;
-    }
-    reader.readAsDataURL(input.files[0]);
-  }
-  else {
-    swal("Sorry - you're browser doesn't support the FileReader API");
+      var reader = new FileReader();
+      reader.onload = function(e) {
+          $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+          // $('#imagePreview').attr("src", 'url('+e.target.result +')');
+          // $('#imagePreview').attr('src', e.target.result);
+          $('#imagePreview').hide();
+          $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
   }
 }
-
-$uploadCrop = $('#upload-demo').croppie({
-  viewport: {
-    width: 150,
-    height: 200,
-  },
-  enforceBoundary: false,
-  enableExif: true
-});
-$('#uploadimageModal').on('shown.bs.modal', function () {
-  // alert('Shown pop');
-  $uploadCrop.croppie('bind', {
-    url: rawImg
-  }).then(function () {
-    console.log('jQuery bind complete');
-  });
+$("#imageUpload").change(function() {
+  readURL(this);
 });
 
-$('.item-img').on('change', function () {
-  imageId = $(this).data('id'); tempFilename = $(this).val();
-  $('#cancelCropBtn').data('id', imageId); readFile(this);
-});
-$('#cropImageBtn').on('click', function (ev) {
-  $uploadCrop.croppie('result', {
-    type: 'base64',
-    format: 'jpeg',
-    size: { width: 150, height: 200 }
-  }).then(function (resp) {
-    $('#item-img-output').attr('src', resp);
-    $('#uploadimageModal').modal('hide');
-  });
-});
-				// End upload preview image
+// function readURL(input) {
+//   if (input.files && input.files[0]) {
+//       var reader = new FileReader();
+
+//       reader.onload = function (e) {
+//           $('#imagePreview')
+//               .attr('src', e.target.result);
+//       };
+
+//       reader.readAsDataURL(input.files[0]);
+//   }
+// }
