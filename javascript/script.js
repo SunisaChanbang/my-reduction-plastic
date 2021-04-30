affe4631fc6f43431e5ed03fbdc03f037d033075
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2021-04-30 17:32:33 +0700

    up

--- javascript/script.js
+++ javascript/script.js
@@ -23,3 +23,57 @@ function editName(x) {
   }
 }
 
+// ///////////////////////////////////
+// Start upload preview image
+$("#choose_image").attr("src", "/pictures/_MG_8982.jpg");
+var $uploadCrop,
+  tempFilename,
+  rawImg,
+  imageId;
+function readFile(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#upload-demo').addClass('ready');
+      $('#uploadimageModal').modal('show');
+      rawImg = e.target.result;
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+  else {
+    swal("Sorry - you're browser doesn't support the FileReader API");
+  }
+}
+
+$uploadCrop = $('#upload-demo').croppie({
+  viewport: {
+    width: 150,
+    height: 200,
+  },
+  enforceBoundary: false,
+  enableExif: true
+});
+$('#uploadimageModal').on('shown.bs.modal', function () {
+  // alert('Shown pop');
+  $uploadCrop.croppie('bind', {
+    url: rawImg
+  }).then(function () {
+    console.log('jQuery bind complete');
+  });
+});
+
+$('.item-img').on('change', function () {
+  imageId = $(this).data('id'); tempFilename = $(this).val();
+  $('#cancelCropBtn').data('id', imageId); readFile(this);
+});
+$('#cropImageBtn').on('click', function (ev) {
+  $uploadCrop.croppie('result', {
+    type: 'base64',
+    format: 'jpeg',
+    size: { width: 150, height: 200 }
+  }).then(function (resp) {
+    $('#item-img-output').attr('src', resp);
+    $('#uploadimageModal').modal('hide');
+  });
+});
+				// End upload preview image
