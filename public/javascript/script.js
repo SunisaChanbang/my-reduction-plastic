2e63f329491cf9d39b70c12d861827762488c407
Chokcolate <61070093@kmitl.ac.th>
2021-05-04 02:44:51 +0700

    tabtab

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -73,3 +73,52 @@ function plusFunction() {
 function minusFunction() {
   document.getElementById("quantity").stepDown();
 }
+
+// // logout
+// const logout = document.querySelector('#logout');
+// logout.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     auth.signOut().then(() => {
+//         console.log('user signed out');
+//     })
+// });
+
+
+//function tqb page
+var tabButtons = document.querySelectorAll(".btn-group button");
+var tabPanels = document.querySelectorAll(".section");
+
+function showPanel(panelIndex) {
+    tabPanels.forEach(function(node){
+        node.style.display="block";
+    });
+    tabPanels[panelIndex].style.display="none";
+}
+showPanel(1);
+
+
+//function Email Validation
+function validation() {
+  var form = document.getElementById("form");
+  var email = document.getElementById("email-su").value;
+  var text = document.getElementById("text")
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "Your Email Address in Valid.";
+    text.style.color = "#00ff00";
+  } else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Please Enter Valid Email Address.";
+    text.style.color = "#ff0000";
+  }
+
+  if (email = "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+    text.style.color = "#00ff00";
+  }
+}
