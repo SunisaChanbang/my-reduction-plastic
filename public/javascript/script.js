9f9d7b62b57dc7b5b4295d0270819126d97078c7
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2021-05-05 03:27:18 +0700

    up

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -16,17 +16,24 @@ function editName(x) {
   var y = document.getElementById("fname").readOnly;
   if (y == true) {
     $("input").prop("readonly", false);
-    document.getElementById("fname").classList.toggle("text-end");
+    // document.getElementById("fname").classList.toggle("text-end");
   } else {
     $("input").prop("readonly", true);
-    document.getElementById("fname").classList.toggle("text-end");
+    // document.getElementById("fname").classList.toggle("text-end");
     var inputName = document.getElementById("fname").value;
     console.log(inputName);
-    const firebaseRef = firebase.database().ref("users".user_now);
-    console.log(firebaseRef);
-    firebaseRef.push({
-      display_name: inputName,
-    });
+    // Add a new document in collection "cities"
+    db.collection("users")
+      .doc(user_now)
+      .set({
+        display_name: inputName,
+      })
+      .then(() => {
+        console.log("เปลี่ยนชื่อสำเร็จ!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: แย่จัง เกิดข้อผิดพลาด", error);
+      });
   }
 }
 
@@ -127,3 +134,29 @@ function validation() {
     text.style.color = "#00ff00";
   }
 }
+// firebase database //
+var plastic_reduction_sum = 20000;
+var bottle_type = 3;
+var bag_type = 5;
+var cup_type = 2;
+var cutlery_type = 8;
+var package_type = 7;
+var straw_type = 9;
+
+let weightAll = ''+(bag_type * 0.2) + (bottle_type * 0.15) + (cup_type * 0.1) + (cutlery_type * 0.6) + (package_type * 0.21) + (straw_type * 0.12);
+
+var nWeight = document.getElementById('nWeight');
+var nBottle = document.getElementById('nBottle');
+var nWatt = document.getElementById('nWatt');
+var nHours = document.getElementById('nHours');
+
+nWeight.innerHTML = weightAll;
+nBottle.innerHTML = bottle_type;
+nWatt.innerHTML = bottle_type * 60;
+nHours.innerHTML = bottle_type * 6;
+console.log("nWeight : " + nWeight.innerHTML);
+console.log("nBottle : " + nBottle.innerHTML);
+console.log("nWatt : " + nWatt.innerHTML);
+console.log("nHours : " + nHours.innerHTML);
+
+
